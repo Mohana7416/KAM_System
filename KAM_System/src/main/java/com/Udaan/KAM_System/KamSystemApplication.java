0ccedb8897759e1424fda47f1bc3f7670209d916
MohanaKrishna <mohana741656@gmail.com>
2024-12-28 19:49:10 +0530

    First Commit

---
+++ KAM_System/src/main/java/com/Udaan/KAM_System/KamSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Udaan.KAM_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KamSystemApplication.class, args);
+	}
+
+}
---
+++ KAM_System/src/test/java/com/Udaan/KAM_System/KamSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Udaan.KAM_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KamSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
